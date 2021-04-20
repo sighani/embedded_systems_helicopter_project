e7e8d9a274b905a8ec0085329b2e39611da11aa0
Nate <22343368+natoopotato@users.noreply.github.com>
2021-04-20 14:42:57 +1200

    I hate docs.

--- src/setup.c
+++ src/setup.c
@@ -15,12 +15,19 @@
 #include "OrbitOLED/OrbitOLEDInterface.h"
 #include "inc/hw_memmap.h"
 #include "labcode/buttons4.h"
-
+#include "driverlib/gpio.h"
 
 
 // Constants
 #define BUF_SIZE 50
 #define SAMPLE_RATE_HZ 500 //By Nyquist Theorem it must be greater than 8, however for polling buttons, this is being done at 500hz
+
+#define YAWI_1 GPIO_INT_PIN_0
+#define YAWI_2 GPIO_INT_PIN_1
+
+#define YAWC_1 GPIO_PIN_0
+#define YAWC_2 GPIO_PIN_1
+
 
 // Global Variables
 circBuf_t g_inBuffer;        // Buffer of size BUF_SIZE integers (sample values)
@@ -34,17 +41,18 @@ void SysTickIntHandler(void) {
 
 void ADCIntHandler(void) {
     uint32_t ulValue;
+    // Clean up, clearing the interrupt
+    ADCIntClear(ADC0_BASE, 3);
+    // ADCIntClear to the first function IAW with reccomendation line 350 of driverlib.c/adc.c
 
-    //
     // Get the single sample from ADC0.  ADC_BASE is defined in
     // inc/hw_memmap.h
     ADCSequenceDataGet(ADC0_BASE, 3, &ulValue);
     //
     // Place it in the circular buffer (advancing write index)
     writeCircBuf(&g_inBuffer, ulValue);
-    //
-    // Clean up, clearing the interrupt
-    ADCIntClear(ADC0_BASE, 3);
+
+
 }
 
 // Intialisation
@@ -100,6 +108,39 @@ void initADC(void) {
     // Enable interrupts for ADC0 sequence 3 (clears any outstanding interrupts)
     ADCIntEnable(ADC0_BASE, 3);
 }
+
+//Yaw Registration
+
+void YawIntHandler(void) {
+    int32_t rawBuffer;
+    uint8_t bufferState;
+
+    GPIOIntClear(GPIO_PORTB_BASE, YAWI_1 | YAWI_2); // First function IAW line 885 driverlib/gpio.c
+
+    rawBuffer = GPIOPinRead(GPIO_PORTB_BASE, (YAWC_1 | YAWC_1));
+    // returning a byte with status of each pin (31 - 8 should be ignored) e.g. .... .... 0000 0010, bits 2+ will remain 0.
+
+    bufferState = rawBuffer & 0x03 // 0000 00XX
+
+}
+
+void initYaw(void) {
+
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+
+//    Wait for port to intialise
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOB))
+    {
+        continue;
+    }
+    GPIOIntRegister(GPIO_PORTB_BASE, YawIntHandler);
+
+    GPIOPinTypeGPIOInput(GPIO_PORTB_BASE, YAWC_1 | YAWC_2);
+    GPIOIntTypeSet(GPIO_PORTB_BASE, YAWC_1 | YAWC_2, GPIO_BOTH_EDGES);
+
+    GPIOIntEnable(GPIO_PORTB_BASE, YAWI_1 | YAWI_2);
+}
+
 
 void initDisplay(void) {
     // intialise the Orbit OLED display
